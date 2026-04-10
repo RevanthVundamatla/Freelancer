2b1fe5b5d50973ff370efa3e1ddd5183e4dddfdf
Revanth Vundamatla <revanthvundamatla@gmail.com>
2026-04-11 00:15:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,4 +18,6 @@ mongoose.connect(process.env.MONGO_URI)
 .catch(err => console.log(err));
 
 // Server
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log("Server running on port " + PORT));
